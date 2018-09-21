fa20cbe35f8f14b93d75c4355a084bf2dd9c9fc8
trushhh <trushboyandriy@gmail.com>
2018-09-21 22:02:16 +0300

    Знайдено методи для отримання юзернеймів користувачів бота

--- Main.py
+++ Main.py
@@ -1,13 +1,13 @@
 import config
-import db
+#import db
 import telebot
 import re
 from telebot import types
 
 
 
-print(db.x.inserted_id)
-print(db.mydict)
+#print(db.x.inserted_id)
+#print(db.mydict)
 
 bot = telebot.TeleBot(config.token)
 digits_pattern = re.compile(r'^[0-9]+$', re.MULTILINE)
@@ -26,6 +26,8 @@ def query_text(query):
     except AttributeError as ex:
         return
     num = matches.group()
+    #user = str(query.from_user.username)
+    #print(user)
     keybroad = types.InlineKeyboardMarkup()
     buttonAccept = types.InlineKeyboardButton(text='Прийняти', callback_data='accept')
     buttonCancel = types.InlineKeyboardButton(text='Відмінити', callback_data='cancel')
@@ -33,19 +35,22 @@ def query_text(query):
     results = []
     msgLend = types.InlineQueryResultArticle(
         id='1', title='Дати в борг',
-        input_message_content=types.InputTextMessageContent(message_text='Надано в борг '+num+' грн.'),
+        input_message_content=types.InputTextMessageContent(message_text='Надано в борг '+num+' грн.\n' + str(query.from_user.username)),
         reply_markup=keybroad
     )
     msgBorrow=types.InlineQueryResultArticle(
         id='2', title='Отримати в борг',
-        input_message_content=types.InputTextMessageContent(message_text='Отримано в борг '+num+' грн.'),
+        input_message_content=types.InputTextMessageContent(message_text='Отримано в борг '+num+' грн.\n' + str(query.from_user.username)),
         reply_markup=keybroad
     )
     msgReturn=types.InlineQueryResultArticle(
         id='3', title='Повернути борг',
-        input_message_content=types.InputTextMessageContent(message_text='Повернено борг в сумі '+num+' грн.'),
+        input_message_content=types.InputTextMessageContent(message_text='Повернено борг в сумі '+num+' грн.\n' + str(query.from_user.username)),
         reply_markup=keybroad
     )
+
+
+
 
     results.append(msgLend)
     results.append(msgReturn)
@@ -57,10 +62,12 @@ def query_text(query):
 def callback_inline(call):
     if call.inline_message_id:
         if call.data == "accept":
-            bot.edit_message_text(inline_message_id=call.inline_message_id, text= '\nПрийнято')
+            bot.edit_message_text(inline_message_id=call.inline_message_id, text= '\nПрийнято \n@' + call.from_user.username)
     if call.inline_message_id:
         if call.data == "cancel":
-            bot.edit_message_text(inline_message_id=call.inline_message_id, text= '\nВідхилено')
+            bot.edit_message_text(inline_message_id=call.inline_message_id, text= '\nВідхилено \n@' + call.from_user.username)
+    #print(bot.get_chat_member(call.chat_id))
+
 
 
 if __name__ == '__main__':
