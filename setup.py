dc1f795f72f6163848a02b0f3b88e79255ba33eb
trushhh <trushboyandriy@gmail.com>
2018-11-04 19:47:45 +0200

    Added setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='DebtsBot',
+    version='0.0.1',
+    packages=[''],
+    url='https://github.com/mtaranovsky/DebtsBot',
+    license='',
+    author='Trush',
+    author_email='trushboyandriy@gmail.com',
+    description=''
+)
