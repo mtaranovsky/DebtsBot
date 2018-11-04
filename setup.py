e8884855c9effc4c8fa0bdc6a4fa3c8d0f501afc
trushhh <trushboyandriy@gmail.com>
2018-11-04 22:14:02 +0200

    Change setup file

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='DebtsBot',
     version='0.0.1',
-    packages=[''],
+    packages=find_packages(),
     url='https://github.com/mtaranovsky/DebtsBot',
     license='',
     author='Trush',
     author_email='trushboyandriy@gmail.com',
-    description=''
+    description='Debts bot for IPS'
 )
