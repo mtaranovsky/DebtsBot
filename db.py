228e525f7f05a98f3f36eff0dbf4f28d25ede251
mtaranovsky <mykolan10@gmail.com>
2018-11-06 01:03:12 +0200

    return to previous version db

--- db.py
+++ db.py
@@ -31,15 +31,23 @@ def request(username,partner,sum):
 
             asd1 = array['debts']
             asd1.append(mydictP)
-            debtUpdate(partner, asd1)
-
+            mycol.update_one(
+                {
+                    'username': partner
+                },
+                {
+                    "$set": {"debts": asd1}})
 
     elif getDebtU == None:
         array = dict(mycol.find_one({'username': username}, {'_id': 0, 'debts': 1}))
         asd = array['debts']
         asd.append(mydictUser)
-        debtUpdate(username,asd)
-
+        mycol.update_one(
+            {
+                'username': username
+            },
+            {
+                "$set": {"debts": asd}})
         if mycol.find_one({'username': partner}, {'_id': 0, 'username': 1}) == None:
             debtsP.append(mydictP)
             y = mycol.insert_one(usersP)
@@ -48,41 +56,38 @@ def request(username,partner,sum):
 
             asd1 = array['debts']
             asd1.append(mydictP)
-            debtUpdate(partner,asd1)
+            mycol.update_one(
+                {
+                    'username': partner
+                },
+                {
+                    "$set": {"debts": asd1}})
 
     else:
-        debtChange(getDebtU,partner,sumoU)
-        debtChange(getDebtP,username,sumoP)
+        for i in dict(getDebtU)['debts']:
+            if i['partner'] == partner:
+                sumoU = i['debt']
 
-    partnerDebtUdate(username,partner,sumoU + sum)
-    partnerDebtUdate(partner, username, sumoP - sum)
+        for i in dict(getDebtP)['debts']:
+            if i['partner'] == username:
+                sumoP = i['debt']
 
-    #
-    for x in mycol.find():
-        print(x)
-
-
-
-def debtChange(Debt,user,sum):
-    for i in dict(Debt)['debts']:
-        if i['partner'] == user:
-            sum = i['debt']
-
-def debtUpdate(username, debt):
-    mycol.update_one(
-        {
-            'username': username
-        },
-        {
-            "$set": {"debts": debt}})
-
-def partnerDebtUdate(username, partner,sum):
     mycol.update_one(
         {
             'username': username, 'debts.partner': partner
         },
         {
-            "$set": {"debts.$.debt": sum, 'debts.$.data': datetime.datetime.now()}})
+            "$set": {"debts.$.debt": sumoU + sum, 'debts.$.data': datetime.datetime.now()}})
+
+    mycol.update_one(
+        {
+            'username': partner, 'debts.partner': username
+        },
+        {
+            "$set": {"debts.$.debt": sumoP - sum, 'debts.$.data': datetime.datetime.now()}})
+    #
+    for x in mycol.find():
+        print(x)
 
 
 def feedback(username):
@@ -98,8 +103,6 @@ def feedback(username):
             a += i["partner"]+" заборгував тобі"+" "+str(i["debt"])+"грн.\n"
 
     return a
-
-
 
 
 
