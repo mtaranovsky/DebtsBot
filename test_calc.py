78f78a86d9a81c628b72d8c1045204f97c6b4342
mtaranovsky <mykolan10@gmail.com>
2018-11-06 01:15:14 +0200

    TEST of mul and div

--- test_calc.py
+++ test_calc.py
@@ -9,5 +9,14 @@ class CalcTest(unittest.TestCase):
     def test_sub(self):
         self.assertEqual(Calc.Calc.sub(self, 4, 2), 2)
 
+    def test_mul(self):
+        self.assertEqual(Calc.Calc.mul(self, 2, 5), 10)
+
+    def test_div(self):
+        self.assertEqual(Calc.Calc.div(self, 8, 2), 4)
+
+
+
+
 if __name__ == '__main__':
     unittest.main()
